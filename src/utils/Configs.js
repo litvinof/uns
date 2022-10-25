2ee3f5374519c102a3870ebcc2f1db2ce1ecb5c5
Viacheslav Litvinov <viacheslavlitvinov@gmail.com>
2022-10-25 06:45:18 +0200

    initial commit

---
+++ src/utils/Configs.js
@@ -0,0 +1,18 @@
+export const Configuration = {
+  backend: {
+    host: 'localhost',
+    port: '56733',
+  },
+  home: {
+    tab_bar_height: 70,
+    initial_show_count: 4,
+    listing_item: {
+      height: 130,
+      offset: 15,
+      saved: {
+        position_top: 5,
+        size: 25,
+      },
+    },
+  }
+};
